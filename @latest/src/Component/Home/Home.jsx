4f342b9de33a7a129a8294353adec7fe594d51c1
HariPrasath1996 <hasathphy96@gmail.com>
2024-09-09 12:51:44 +0530

    nav bar created

--- @latest/src/Component/Home/Home.jsx
+++ @latest/src/Component/Home/Home.jsx
@@ -1,25 +1,27 @@
 import React from 'react'
 import "./Home.css"
-import {Link}  from "react-router-dom"
+import { Link } from "react-router-dom"
 const Home = () => {
     return (
         <>
-            <div className='img-container'>
-                <img className='logo' src="src/image/vamp-logo.png" alt="vamp-logo"  />
-            </div>
-            <div className='contain'>
-                <ul className='un-order'>
-                    <li>
-                        <Link to={"/"} className='Home'>Home </Link>
-                    </li>
-                    <li>
-                        <Link to={"Service"}>Service</Link>
-                    </li>
-                    <li>
-                        <Link to={"Contact"} className='Contact_us'>Contact_us</Link>
-                    </li>
-                </ul>
-            </div>
+            <nav className='nav-bar'>
+                <div className='img-container'>
+                    <img className='logo' src="src/image/vamp-logo.png" alt="vamp-logo" />
+                </div>
+                <div className='contain'>
+                    <ul className='un-order'>
+                        <li>
+                            <Link to={"/"} className='Home'>Home </Link>
+                        </li>
+                        <li>
+                            <Link to={"Service"}>Service</Link>
+                        </li>
+                        <li>
+                            <Link to={"Contact"} className='Contact_us'>Contact_us</Link>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
         </>
     )
 }
